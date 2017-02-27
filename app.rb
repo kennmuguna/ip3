2a34307e34f9bb1b7af09d4b8b1f12ffec04a501
kennmuguna <kennmuguna@gmail.com>
2017-02-27 15:26:05 +0300

    persistent unidentified error

--- app.rb
+++ app.rb
@@ -2,6 +2,23 @@ require("sinatra")
   require("sinatra/reloader")
   also_reload("lib/**/*.rb")
   require("./lib/stylist")
+  require("./lib/client")
+  also_reload("lib/**/*.rb")
   require("pg")
 
   DB = PG.connect({:dbname => "hair_salon"})
+
+  get("/") do
+    erb(:index)
+  end
+
+  get("/stylists/new") do
+    erb(:stylist_form)
+  end
+
+  post("/stylists") do
+    name = params.fetch("name")
+    stylist = Stylist.new({:name => name, :id => nil})
+    stylist.save()
+    erb(:stylist_success)
+  end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,3 +1,14 @@
 require('capybara/rspec')
   require('./app')
-  
+  Capybara.app = Sinatra::Application
+  set(:show_exceptions, false)
+
+  describe('adding a new stylist', {:type => :feature}) do
+      it('allows a user to click a stylyst and see the clients & details for them') do
+          visit('/')
+          click_link('Add New Stylist')
+          fill_in('name', :with => 'Khadija')
+          click_button('Add Stylist')
+          expect(page).to have_content('Success')
+        end
+      end
